76ab5fd0bdb2570bf42dabee637d4b796609fee6
Mhilmy11 <setiawantojr@gmail.com>
2026-06-04 11:07:48 +0700

    optimization landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -159,7 +159,7 @@ export default function LandingPage() {
 
       <div className=" bg-yellow-500 text-white">
         <Container>
-          <div className=" mx-20 py-10 md:flex items-start gap-20">
+          <div className=" grid grid-cols-1 md:grid-cols-2 gap-10 py-8 px-4">
             <div>
               <h6 className=" md:text-5xl text-3xl font-bold mb-5">
                 Pelatihan <span className=" text-secondary">pajak praktis</span>{" "}
@@ -203,10 +203,10 @@ export default function LandingPage() {
                 </div>
               </div>
 
-              <div className=" mt-7 md:flex gap-4 md:text-xl font-semibold">
+              <div className=" mt-7 grid grid-cols-2 gap-4 md:text-xl font-semibold">
                 <button
                   onClick={handleClickToProduct}
-                  className=" md:mb-0 mb-3 cursor-pointer hover:bg-blue-950 hover:border-blue-950 hover:text-yellow-400 transition flex gap-2 items-center border-2 rounded-xl py-3 px-4"
+                  className=" cursor-pointer hover:bg-blue-950 hover:border-blue-950 hover:text-yellow-400 transition flex gap-2 items-center border-2 rounded-xl py-3 px-4"
                 >
                   Lihat Program <AiOutlineArrowDown size={25} />
                 </button>
