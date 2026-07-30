fcdafcb85b9df1827bf502abac5c8604b99adaa6
Mhilmy11 <setiawantojr@gmail.com>
2026-07-30 10:08:51 +0700

    D2P Refactor code structure for improved readability and maintainability

--- assets/LazyImage-B7XpxxDn.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./index-B6Y39LAx.js";function s({src:a,alt:o,className:r,width:t,height:i}){return m.jsx("img",{src:a,alt:o,loading:"lazy",width:t,height:i,className:r})}export{s as L};
---
+++ assets/LazyImage-FB_Ur-on.js
@@ -0,0 +1,1 @@
+import{j as m}from"./index-CHKYYOlD.js";function s({src:a,alt:o,className:r,width:t,height:i}){return m.jsx("img",{src:a,alt:o,loading:"lazy",width:t,height:i,className:r})}export{s as L};
